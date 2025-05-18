d04fbdc34308c96c29fabd2b26f6c34e6d8f181a
Steven De wolf <stevendewolf1983@icloud.com>
2025-05-18 19:13:21 +0200

    Create coins.js

---
+++ src/config/coins.js
@@ -0,0 +1,17 @@
+export const coins = [
+  {
+    id: "bitcoin",
+    symbol: "BTC",
+    name: "Bitcoin",
+  },
+  {
+    id: "ripple",
+    symbol: "XRP",
+    name: "Ripple",
+  },
+  {
+    id: "livepeer",
+    symbol: "LPT",
+    name: "Livepeer",
+  },
+];
